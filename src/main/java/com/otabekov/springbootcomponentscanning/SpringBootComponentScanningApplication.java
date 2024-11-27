efa9f9f1366e01001f819b6d8e20bb6b928b13b8
Asadbek Otabekov <otabekovasadbek6@gmail.com>
2024-11-28 02:35:51 +0500

    first commit

---
+++ src/main/java/com/otabekov/springbootcomponentscanning/SpringBootComponentScanningApplication.java
@@ -0,0 +1,13 @@
+package com.otabekov.springbootcomponentscanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComponentScanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootComponentScanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otabekov/springbootcomponentscanning/SpringBootComponentScanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otabekov.springbootcomponentscanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComponentScanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
